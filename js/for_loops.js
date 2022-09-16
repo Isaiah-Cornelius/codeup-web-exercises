8e5b436a644f244e6df213f95a58a8a8fe6ad37d
Isaiah Cornelius <isaiah.cornelius01@gmail.com>
2022-09-16 13:49:03 -0600

    Completed For Loops Exercise #3

--- js/for_loops.js
+++ js/for_loops.js
@@ -10,8 +10,23 @@ function showMultiplicationTable(x){
     }
 }
 
-
-
+//Question #3
+function tenRandomNumbersOddOrEven(){
+    for(let i = 0; i<10; i++){
+        function getRandomInt(min, max){
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min) + min);
+        }
+        let randomNumber = getRandomInt(20,99);
+        if(randomNumber % 2 === 0){
+            console.log(randomNumber + " is even.");
+        } else {
+            console.log(randomNumber + " is odd.");
+        }
+        continue;
+    }
+}
 
 
 
