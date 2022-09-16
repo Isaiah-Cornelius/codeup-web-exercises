0e4cc0a3e7e180fea56739bf1bf5b32bd2ccb068
Isaiah Cornelius <isaiah.cornelius01@gmail.com>
2022-09-16 14:25:52 -0600

    Completed For Loops Exercise Question #4

--- js/for_loops.js
+++ js/for_loops.js
@@ -28,6 +28,15 @@ function tenRandomNumbersOddOrEven(){
     }
 }
 
+//For Loop Question #4
+let string = "";
+for(let i=1; i < 10; i++){
+    for(let x=1; x<=i; x++){
+        string+=i;
+    }
+    string+="\n";
+}
+console.log(string);
 
 
 
