f8533323220af186d89d7a40102367826ff8020d
Isaiah Cornelius <isaiah.cornelius01@gmail.com>
2022-09-16 13:04:20 -0600

    Completed Loops Exercise Question #2

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,1 +1,55 @@
 "use strict";
+
+
+//For Loops Exercise
+
+//Question #2
+function showMultiplicationTable(x){
+    for(let i = 1; i <= 10; i++){
+        console.log(x + " x " + i + " = " + (x*i));
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
