c8c33e12c21f79363fd3c4792c4e9bd06ca38a86
Isaiah Cornelius <isaiah.cornelius01@gmail.com>
2022-09-20 14:48:50 -0600

    Completed books array callback function to console.log  book number, title, and author

--- js/objects.js
+++ js/objects.js
@@ -138,7 +138,13 @@
      *      ---
      *      ...
      */
+    books.forEach(function (book, index){
+        console.log("Book # " + (index + 1));
+        console.log("Title: " + book.title);
+        console.log("Author: " + book.author.firstName + " " + book.author.lastName);
+        console.log("---");
 
+    })
     /**
      * Bonus:
      * - Create a function named `createBook` that accepts a title and author
