f7282fbda64d19962e453b00a8bd74aef1debb36
Isaiah Cornelius <isaiah.cornelius01@gmail.com>
2022-09-20 08:19:26 -0600

    Completed planets-string.js

--- js/planets-string.js
+++ js/planets-string.js
@@ -1,8 +1,8 @@
 (function(){
     "use strict";
 
-    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
-    var planetsArray;
+    let planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+
 
     /**
      * TODO:
@@ -10,18 +10,36 @@
      * planetsArray.
      * console.log planetsArray to check your work
      */
-
-    // console.log(planetsArray);
+    let planetsArray = planetsString.split('|');
+    console.log(planetsArray);
 
     /**
      * TODO:
      * Create a string with <br> tags between each planet. console.log() your
      * results. Why might this be useful?
-     *
+     */
+    let planetsStringWithBreaks = "";
+    for (let i = 0; i < planetsArray.length; i++){
+        planetsStringWithBreaks += planetsArray[i] + ' <br> ';
+    }
+    console.log(planetsStringWithBreaks);
+
+
+     /**
      * BONUS:
      * Create another string that would display your planets in an undordered
      * list. You will need an opening AND closing <ul> tags around the entire
      * string, and <li> tags around each planet.
      */
-
+    let planetsStringUnorderedList = "";
+    for(let i = 0; i < planetsArray.length; i++){
+        if (i === 0){
+            planetsStringUnorderedList += '<ul> ';
+        }
+        planetsStringUnorderedList += ' <li> ' + planetsArray[i] + ' </li> ';
+        if (i === planetsArray.length -1){
+            planetsStringUnorderedList += ' </ul>';
+        }
+    }
+    console.log(planetsStringUnorderedList);
 })();
