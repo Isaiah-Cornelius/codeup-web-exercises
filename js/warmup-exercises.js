56626b935fa63c5b3fd416b505c83a8c0d678b46
Isaiah Cornelius <isaiah.cornelius01@gmail.com>
2022-09-26 10:14:46 -0600

    Completed 9/26/22 Warmup Exercises

--- js/warmup-exercises.js
+++ js/warmup-exercises.js
@@ -1,43 +1,75 @@
 "use strict";
 
-//// 9 24/22
-/**
- * Randomize an array in-place using an algorithm (Modified Durstenfeld Algoritm)
- */
-function shuffleArray(array){
-    //create a blank array to push to when the loop index and the random index are the same; no shuffle
-    let noChangeIndex = [];
-    console.log("Original array is : " + array);
-    //this for loop will shuffle the entire array twice per index; once from the back forward and once from the front backwards
-    for (let i = array.length -1, a = 0; i > -1; i--, a++ ){
-        // console.log("i = :" + i);
-        let j = Math.floor(Math.random() * (array.length));
-        // console.log("j = :" + j);
-        // console.log("a = :" + a);
-        let b = Math.floor(Math.random() * (array.length));
-        // console.log("b = :" + b);
-        let temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-        let temp2 = array[a];
-        array[a] = array[b];
-        array[b] = temp2;
-        // console.log("-----")
-        //the following two conditional statements push to the noChangeIndex array when a shuffle fails; meaning the used index and the random index are the same and nothing actually changed within the array
-        if (i === j){
-                    noChangeIndex.push("j is i for [" + i + "]");
-        }
-        if (a === b){
-            noChangeIndex.push("a is b for [" + a + "]");
-        }
-    }
-    console.log("No change on index : " + noChangeIndex);
-    console.log(((((array.length * 2) - noChangeIndex.length) / (array.length * 2)) * 100).toFixed(2)+ "% actual shuffle");
-    console.log(noChangeIndex.length + " Matching shuffles");
-    console.log(((array.length * 2) - noChangeIndex.length) + " properly shuffled index actions out of " + (array.length *2) + " trys.");
-    return array;
-}
-let array = [1,2,3,4,5,6];
+
+//// 9/26/22
+// Write a function that takes an array of objects and a string as arguments. Add a property with key ‘continent’ and value equal to the string to each of the objects. Return the new array of objects. Don’t mutate the original array.
+// example input: [{ city: 'Tokyo', country: 'Japan' }, { city: 'Bangkok', country: 'Thailand' }], 'Asia'
+// expected output: [{ city: 'Tokyo', country: 'Japan', continent: 'Asia' }, { city: 'Bangkok', country: 'Thailand', continent: 'Asia' }]
+// function returnArrayOfObjectsWithContinent(x,y){
+//     if (Array.isArray(x)){
+//         if(typeof y === 'string'){
+//             x.forEach(function (element){
+//                 element.continent = y
+//             })
+//             return x;
+//         } return "Invalid input; please enter the continent as a string as the second argument"
+//     } return "Invalid input; please enter an array as the first argument"
+// }
+//
+// Write a function to return an array of only the Even numbers from an array with the numbers 1 through 10
+// example input: [1,2,3,4,5] expected output: [2,4]
+//
+// function returnOnlyEven(x){
+//     if (Array.isArray(x)){
+//         let newArray = [];
+//         x.forEach(function(element){
+//             if (element % 2 === 0){
+//                 newArray.push(element);
+//             }
+//         })
+//         return newArray;
+//     } return "Invalid input; please enter an array"
+// }
+
+
+//// 9/24/22
+// /**
+//  * Randomize an array in-place using an algorithm (Modified Durstenfeld Algoritm)
+//  */
+// function shuffleArray(array){
+//     //create a blank array to push to when the loop index and the random index are the same; no shuffle
+//     let noChangeIndex = [];
+//     console.log("Original array is : " + array);
+//     //this for loop will shuffle the entire array twice per index; once from the back forward and once from the front backwards
+//     for (let i = array.length -1, a = 0; i > -1; i--, a++ ){
+//         // console.log("i = :" + i);
+//         let j = Math.floor(Math.random() * (array.length));
+//         // console.log("j = :" + j);
+//         // console.log("a = :" + a);
+//         let b = Math.floor(Math.random() * (array.length));
+//         // console.log("b = :" + b);
+//         let temp = array[i];
+//         array[i] = array[j];
+//         array[j] = temp;
+//         let temp2 = array[a];
+//         array[a] = array[b];
+//         array[b] = temp2;
+//         // console.log("-----")
+//         //the following two conditional statements push to the noChangeIndex array when a shuffle fails; meaning the used index and the random index are the same and nothing actually changed within the array
+//         if (i === j){
+//                     noChangeIndex.push("j is i for [" + i + "]");
+//         }
+//         if (a === b){
+//             noChangeIndex.push("a is b for [" + a + "]");
+//         }
+//     }
+//     console.log("No change on index : " + noChangeIndex);
+//     console.log(((((array.length * 2) - noChangeIndex.length) / (array.length * 2)) * 100).toFixed(2)+ "% actual shuffle");
+//     console.log(noChangeIndex.length + " Matching shuffles");
+//     console.log(((array.length * 2) - noChangeIndex.length) + " properly shuffled index actions out of " + (array.length *2) + " trys.");
+//     return array;
+// }
+// let array = [1,2,3,4,5,6];
 
 //// 9/23/22
 /* Randomize array in-place using Durstenfeld shuffle algorithm */
