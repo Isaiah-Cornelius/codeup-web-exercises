bbd3592a479d58988fb7f53ea5fdd81bc1735b7a
Isaiah Cornelius <isaiah.cornelius01@gmail.com>
2022-10-19 16:25:39 -0600

    Completed jQuery Selectors Element Selectors Question #4

--- js/my_custom_JS.js
+++ js/my_custom_JS.js
@@ -2,9 +2,11 @@ $(function (){
     $('li').css('fontSize', '20px')
     $('h1, p, li').css('background-color', '#FF0');
 
+let h1Elements = $('h1')
 
-
-
+for (let i = 0; i < h1Elements.length; i++){
+    alert(h1Elements[i].innerText);
+}
 
 
 
