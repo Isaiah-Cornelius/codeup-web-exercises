837add9f51a5c58eaacb01779f1a91b212e27e20
Isaiah Cornelius <isaiah.cornelius01@gmail.com>
2022-10-31 16:39:27 -0600

    Completed all but bonus map filter reduce exercises

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -1,6 +1,5 @@
 "use strict";
 
-alert('hi')
 const users = [
     {
         id: 1,
@@ -38,3 +37,38 @@ const users = [
         yearsOfExperience: 9
     }
 ];
+
+// Use .filter to create an array of user objects where each user object has at least 3 languages in the languages array.
+let hasAtLeastThreeLanguages = users.filter(function (user){
+    return user.languages.length >= 3;
+})
+
+// Use .map to create an array of strings where each element is a user's email address
+let emailsArray = users.map(function (user){
+    return user.email
+})
+
+// Use .reduce to get the total years of experience from the list of users.
+let totalYearsOfExperience = users.reduce(function (accum, user){
+    return accum + user.yearsOfExperience
+}, 0)
+
+// Once you get the total of years you can use the result to calculate the average.
+let averageYearsOfExperience = totalYearsOfExperience / users.length
+
+//     Use .reduce to get the longest email from the list of users.
+let longestEmail = users.reduce(function (accum, user){
+    if (user.email.split("").length > accum.split("").length){
+        accum = user.email
+    }
+    return accum
+}, '')
+
+//     Use .reduce to get the list of user's names in a single string. Example: Your instructors are: ryan, luis, zach, fernando, justin.
+
+let usersNames = users.reduce(function (accum, user){
+    accum += user.name + ", "
+    return accum
+},"")
+
+// Use .reduce to get the unique list of languages from the list of users.
